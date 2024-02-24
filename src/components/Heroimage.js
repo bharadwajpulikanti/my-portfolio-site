4accca67c3fb10b87168a1883299f6e1c70d0514
bharadwajpulikanti <bharadwajpulikanti25@gmail.com>
2024-02-24 19:18:57 +0530

    updated hero section code

--- src/components/Heroimage.js
+++ src/components/Heroimage.js
@@ -1,1 +1,28 @@
 import classes from "./Heroimage.module.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import IntroImg from "../assets/intro-bg.jpg";
+
+const Heroimage = () => {
+  return (
+    <div className={classes.hero}>
+      <div className={classes.mask}>
+        <img className={classes.intoimg} src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className={classes.content}>
+        <p>HEY, I'M</p>
+        <h1>Bharadwaj Pulikanti</h1>
+        <div>
+          <Link to="/projects" className="btn">
+            PROJECTS
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroimage;

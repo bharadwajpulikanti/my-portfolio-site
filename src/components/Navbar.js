13c0b089b570a3ea03f9ec8f22343e912a1ad894
bharadwajpulikanti <bharadwajpulikanti25@gmail.com>
2024-02-24 15:15:25 +0530

    Updated the code upto responsive navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import classes from "./Navbar.module.css";
+
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  return (
+    <div className={classes.header}>
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul
+        className={
+          click ? `${classes.navmenu} ${classes.active}` : classes.navmenu
+        }
+      >
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className={classes.hamburger} onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
